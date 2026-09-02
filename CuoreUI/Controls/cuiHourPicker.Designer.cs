4b23156152b0d7fe10040c7985a9ca934356e662
SMoonKnight <satriacandrasmk@gmail.com>
2026-09-02 07:33:51 +0700

    update

---
+++ CuoreUI/Controls/cuiHourPicker.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace CuoreUI.Controls
+{
+    partial class cuiHourPicker
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.hourTextBox = new CuoreUI.Controls.cuiTextBox();
+            this.minuteTextBox = new CuoreUI.Controls.cuiTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(54, 2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 15);
+            this.label1.TabIndex = 35;
+            this.label1.Text = ":";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // hourTextBox
+            // 
+            this.hourTextBox.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(37)))), ((int)(((byte)(42)))));
+            this.hourTextBox.Content = "";
+            this.hourTextBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.hourTextBox.FocusBackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(37)))), ((int)(((byte)(42)))));
+            this.hourTextBox.FocusImageTint = System.Drawing.Color.White;
+            this.hourTextBox.FocusOutlineColor = System.Drawing.Color.Gray;
+            this.hourTextBox.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hourTextBox.ForeColor = System.Drawing.Color.Gray;
+            this.hourTextBox.Image = null;
+            this.hourTextBox.ImageExpand = new System.Drawing.Point(0, 0);
+            this.hourTextBox.ImageOffset = new System.Drawing.Point(0, 0);
+            this.hourTextBox.Location = new System.Drawing.Point(2, -1);
+            this.hourTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.hourTextBox.Multiline = false;
+            this.hourTextBox.Name = "hourTextBox";
+            this.hourTextBox.NormalImageTint = System.Drawing.Color.White;
+            this.hourTextBox.OutlineColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.hourTextBox.Padding = new System.Windows.Forms.Padding(16, 2, 16, 0);
+            this.hourTextBox.PasswordChar = false;
+            this.hourTextBox.PlaceholderColor = System.Drawing.Color.LightGray;
+            this.hourTextBox.PlaceholderText = "00";
+            this.hourTextBox.Rounding = new System.Windows.Forms.Padding(8);
+            this.hourTextBox.Size = new System.Drawing.Size(49, 21);
+            this.hourTextBox.TabIndex = 36;
+            this.hourTextBox.TextOffset = new System.Drawing.Size(0, 0);
+            this.hourTextBox.UnderlinedStyle = true;
+            // 
+            // minuteTextBox
+            // 
+            this.minuteTextBox.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(37)))), ((int)(((byte)(42)))));
+            this.minuteTextBox.Content = "";
+            this.minuteTextBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.minuteTextBox.FocusBackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(37)))), ((int)(((byte)(42)))));
+            this.minuteTextBox.FocusImageTint = System.Drawing.Color.White;
+            this.minuteTextBox.FocusOutlineColor = System.Drawing.Color.Gray;
+            this.minuteTextBox.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.minuteTextBox.ForeColor = System.Drawing.Color.Gray;
+            this.minuteTextBox.Image = null;
+            this.minuteTextBox.ImageExpand = new System.Drawing.Point(0, 0);
+            this.minuteTextBox.ImageOffset = new System.Drawing.Point(0, 0);
+            this.minuteTextBox.Location = new System.Drawing.Point(69, -1);
+            this.minuteTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.minuteTextBox.Multiline = false;
+            this.minuteTextBox.Name = "minuteTextBox";
+            this.minuteTextBox.NormalImageTint = System.Drawing.Color.White;
+            this.minuteTextBox.OutlineColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.minuteTextBox.Padding = new System.Windows.Forms.Padding(16, 2, 16, 0);
+            this.minuteTextBox.PasswordChar = false;
+            this.minuteTextBox.PlaceholderColor = System.Drawing.Color.LightGray;
+            this.minuteTextBox.PlaceholderText = "00";
+            this.minuteTextBox.Rounding = new System.Windows.Forms.Padding(8);
+            this.minuteTextBox.Size = new System.Drawing.Size(49, 21);
+            this.minuteTextBox.TabIndex = 37;
+            this.minuteTextBox.TextOffset = new System.Drawing.Size(0, 0);
+            this.minuteTextBox.UnderlinedStyle = true;
+            // 
+            // cuiHourPicker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Transparent;
+            this.Controls.Add(this.minuteTextBox);
+            this.Controls.Add(this.hourTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "cuiHourPicker";
+            this.Size = new System.Drawing.Size(120, 20);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private cuiTextBox hourTextBox;
+        private cuiTextBox minuteTextBox;
+    }
+}
---
+++ CuoreUI/Controls/cuiHourPicker.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Media;
+using System.Windows.Forms;
+
+namespace CuoreUI.Controls
+{
+    public partial class cuiHourPicker : UserControl
+    {
+        private TimeSpan privateValue = TimeSpan.Zero;
+        private bool updating;
+
+        [Category("CuoreUI")]
+        [Browsable(true)]
+        public TimeSpan Value
+        {
+            get => privateValue;
+            set
+            {
+                SetValue(value);
+
+                if (IsHandleCreated)
+                {
+                    UpdateTextBoxes();
+                }
+            }
+        }
+
+        [Category("CuoreUI")]
+        [Browsable(true)]
+        public event EventHandler ValueChanged;
+
+        [Category("CuoreUI")]
+        [Browsable(true)]
+        public new Color ForeColor
+        {
+            get => hourTextBox.ForeColor;
+            set
+            {
+                hourTextBox.ForeColor = value;
+                minuteTextBox.ForeColor = value;
+            }
+        }
+
+        [Category("CuoreUI")]
+        [Browsable(true)]
+        public Color FocusForeColor
+        {
+            get => focusForeColor;
+            set
+            {
+                focusForeColor = value;
+                UpdateForeColors();
+            }
+        }
+
+        private Color focusForeColor;
+
+        private void UpdateForeColors()
+        {
+            hourTextBox.ForeColor =
+                hourTextBox.Focused
+                    ? focusForeColor
+                    : ForeColor;
+
+            minuteTextBox.ForeColor =
+                minuteTextBox.Focused
+                    ? focusForeColor
+                    : ForeColor;
+        }
+
+        [Category("CuoreUI")]
+        [Browsable(true)]
+        [DefaultValue(false)]
+        public bool ReadOnly { get; set; }
+
+        public cuiHourPicker()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            TabStop = true;
+
+            hourTextBox.ContentChanged += HourTextBox_ContentChanged;
+            minuteTextBox.ContentChanged += MinuteTextBox_ContentChanged;
+
+            hourTextBox.KeyPress += TextBox_KeyPress;
+            minuteTextBox.KeyPress += TextBox_KeyPress;
+
+            UpdateTextBoxes();
+        }
+
+        private void UpdateTextBoxes()
+        {
+            updating = true;
+
+            hourTextBox.Text = privateValue.Hours.ToString("D2");
+            minuteTextBox.Text = privateValue.Minutes.ToString("D2");
+
+            updating = false;
+        }
+
+        private void HourTextBox_ContentChanged(object sender, EventArgs e)
+        {
+            if (ReadOnly)
+            {
+                UpdateTextBoxes();
+                return;
+            }
+
+            if (updating || hourTextBox.Text.Length < 2)
+            {
+                return;
+            }
+
+            if (!int.TryParse(hourTextBox.Text, out int hour) || hour > 23)
+            {
+                SystemSounds.Beep.Play();
+
+                updating = true;
+                hourTextBox.Text = "";
+                updating = false;
+
+                hourTextBox.Focus();
+                return;
+            }
+
+            if (int.TryParse(minuteTextBox.Text, out int minute))
+            {
+                UpdateValue(hour, minute);
+            }
+
+            minuteTextBox.Focus();
+        }
+
+        private void MinuteTextBox_ContentChanged(object sender, EventArgs e)
+        {
+            if (ReadOnly)
+            {
+                UpdateTextBoxes();
+                return;
+            }
+
+            if (updating || minuteTextBox.Text.Length < 2)
+            {
+                return;
+            }
+
+            if (!int.TryParse(minuteTextBox.Text, out int minute) || minute > 59)
+            {
+                SystemSounds.Beep.Play();
+
+                updating = true;
+                minuteTextBox.Text = "";
+                updating = false;
+
+                minuteTextBox.Focus();
+                return;
+            }
+
+            if (int.TryParse(hourTextBox.Text, out int hour))
+            {
+                UpdateValue(hour, minute);
+            }
+        }
+
+        private void TextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (ReadOnly)
+            {
+                e.Handled = true;
+                return;
+            }
+
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+                SystemSounds.Beep.Play();
+            }
+        }
+
+        private void UpdateValue(int hour, int minute)
+        {
+            SetValue(new TimeSpan(hour, minute, 0));
+        }
+
+        private void SetValue(TimeSpan value)
+        {
+            if (value < TimeSpan.Zero || value >= TimeSpan.FromDays(1))
+            {
+                value = TimeSpan.Zero;
+            }
+            else
+            {
+                value = new TimeSpan(value.Hours, value.Minutes, 0);
+            }
+
+            if (privateValue == value)
+            {
+                return;
+            }
+
+            privateValue = value;
+            ValueChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
